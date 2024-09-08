5a76fd9ed3f1fa78f5fede44517057d08b1a88d8
Austin Grisham <grishama@gmatc.matc.edu>
2024-09-08 00:58:17 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 import express from 'express';
+import connectDatabase from './config/db.js';
+import { connect } from 'mongoose';
 
 const app = express();
-const port = 3001;
+connectDatabase();
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to the API',
-        data: {
-            info: 'This is the root API endpoint',
-            version: '1.0.0'
-        }
-    })
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${3001}`);
+app.get('/', (req, res) => 
+    res.send('http get request sent to root api endpoint')
+);
+app.listen(3001, () => console.log('Express server running on port 3001'))
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
