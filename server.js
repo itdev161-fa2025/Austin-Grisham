7843a6886e31d4af518ca612865bc40743495132
Austin Grisham <grishama@gmatc.matc.edu>
2024-09-12 22:53:45 -0500

    Activity 3

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
-import { connect } from 'mongoose';
 
 const app = express();
 connectDatabase();
 
-app.get('/', (req, res) => 
+// Middleware to parse incoming JSON
+app.use(express.json());
+
+app.get('/', (_req, res) => 
     res.send('http get request sent to root api endpoint')
 );
-app.listen(3001, () => console.log('Express server running on port 3001'))
+
+// POST route to log and return the request body
+app.post('/user', (req, res) => {
+    console.log(req.body);  // Log the request body
+    res.json(req.body);     // Return the request body as a response
+});
+
 // Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, _req, res) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
+app.listen(3001, () => console.log('Express server running on port 3001'));
