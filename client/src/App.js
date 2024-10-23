00face5f15c5a724cc0144b3f5b3f1570fcf7804
Austin Grisham <grishama@gmatc.matc.edu>
2024-10-22 21:44:07 -0500

    Activity 7

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,79 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { Routes, Route, Link } from 'react-router-dom';
+
+// Components for different pages
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to GoodThings</h2>
+      <p>This is the Home page.</p>
+    </div>
+  );
+}
+
+function Register() {
+  return (
+    <div>
+      <h2>Register Page</h2>
+      <p>Please register for an account here.</p>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>Enter your credentials to log in.</p>
+    </div>
+  );
+}
 
 class App extends React.Component {
   state = {
     data: null
-  }
+  };
 
   componentDidMount() {
     axios.get('http://localhost:3000')
       .then((response) => {
         this.setState({
           data: response.data
-        })        
+        });
       })
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
-      })
+      });
   }
+
   render() {
     return (
-    <div className='App'>
-      <header className='App-header'>
-        GoodThings
-      </header>
-    </div>
+      <div className='App'>
+        <header className='App-header'>
+          {/* Title */}
+          <h1>GoodThings</h1>
+
+          {/* Navigation Links */}
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/register">Register</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        {/* Define Routes */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
     );
   }
 }
