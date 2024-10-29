2661b9439b7aebd3be55fe55ed9c8b83b3767d3e
Austin Grisham <grishama@gmatc.matc.edu>
2024-10-28 22:55:19 -0500

    Add user authentication and authorization methods in App.js with logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home'; // Corrected Import
-import Register from './components/Register'; // Corrected Import
-import Login from './components/Login'; // Corrected Import
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 class App extends React.Component {
   state = {
-    data: null
+    data: null,
+    token: null,
+    user: null
   };
 
   componentDidMount() {
@@ -21,31 +23,74 @@ class App extends React.Component {
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
       });
+
+    this.authenticateUser();
   }
 
+  authenticateUser = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      localStorage.removeItem('user');
+      this.setState({ user: null });
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+
+      axios.get('http://localhost:3000/api/auth', config)
+        .then((response) => {
+          localStorage.setItem('user', response.data.name);
+          this.setState({ user: response.data.name });
+        })
+        .catch((error) => {
+          localStorage.removeItem('user');
+          this.setState({ user: null });
+          console.error(`Error logging in: ${error}`);
+        });
+    }
+  };
+
+  logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null });
+  };
+
   render() {
+    const { user, data } = this.state;
+    const authProps = { authenticateUser: this.authenticateUser };
+
     return (
       <div className='App'>
         <header className='App-header'>
-          {/* Title */}
           <h1>GoodThings</h1>
 
-          {/* Navigation Links */}
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li> {/* Changed path to "/" */}
-              <li><Link to="/register">Register</Link></li> {/* Lowercase */}
-              <li><Link to="/login">Login</Link></li> {/* Lowercase */}
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/register">Register</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li>
+                {user ? (
+                  <button onClick={this.logOut}>Log out</button>
+                ) : (
+                  <Link to="/login">Log in</Link>
+                )}
+              </li>
             </ul>
           </nav>
         </header>
 
-        {/* Define Routes */}
         <main>
           <Routes>
-            <Route path="/" element={<Home />} /> {/* Home path is "/" */}
-            <Route path="/register" element={<Register />} /> {/* Lowercase */}
-            <Route path="/login" element={<Login />} /> {/* Lowercase */}
+            <Route path="/" element={<Home user={user} data={data} />} />
+            <Route path="/register" element={<Register {...authProps} />} />
+            <Route path="/login" element={<Login {...authProps} />} />
           </Routes>
         </main>
       </div>
